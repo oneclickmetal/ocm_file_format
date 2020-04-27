df22b28ed8bf48cb19adba4624dd70f9910a87b6
Matthias Harrer <matthias.harrer@oneclickmetal.com>
2020-04-27 16:53:11 +0200

    add example for extracting information from package

--- lib/ocm_file_format/__main__.py
+++ lib/ocm_file_format/__main__.py
@@ -1,6 +1,22 @@
-from . import OCMFile
+import sys
+from . import OCMFile, GCODE_RELATION_TYPE, THUMBNAIL_RELATION_TYPE, JOB_PARAMETERS_RELATION_TYPE, JOB_DESCRIPTION_RELATION_TYPE
 
 if __name__ == '__main__':
-    with OCMFile('tests/test.ocm') as ocmfile:
-        params = ocmfile.read_job_description('/3D/model.mprint_job_description')
-        print(params)
+    if len(sys.argv) != 2:
+        print("Please provide the file path to the .ocm file as argument")
+        exit(1)\
+
+    with OCMFile(sys.argv[1]) as ocmfile:
+        gcode_part = ocmfile.read_relationships('/', type=GCODE_RELATION_TYPE)[0].Target
+
+        parameters_part = ocmfile.read_relationships(gcode_part, JOB_PARAMETERS_RELATION_TYPE)[0].Target
+        description_part = ocmfile.read_relationships(gcode_part, JOB_DESCRIPTION_RELATION_TYPE)[0].Target
+        thumbnail_part = ocmfile.read_relationships(gcode_part, THUMBNAIL_RELATION_TYPE)[0].Target
+
+        parameters = ocmfile.read_job_parameters(parameters_part)
+        description = ocmfile.read_job_description(description_part)
+
+        print("Gcode path: ", gcode_part)
+        print("Thumbnail path: ", thumbnail_part)
+        print("Parameters: ", parameters)
+        print("Description: ", description)
