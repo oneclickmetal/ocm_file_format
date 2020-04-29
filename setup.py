9d0eb710c17f64977e8f405cf16ed9b2d96b93ff
Matthias Harrer <matthias.harrer@oneclickmetal.com>
2020-04-29 10:23:47 +0200

    use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
-from distutils.core import setup
-setup(name='ocm_file_format',
-      version='0.0.1',
-      py_modules=['ocm_file_format'],
-      )
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ocm_file_format',
+    version='0.0.1',
+
+    author="Matthias Harrer",
+    author_email="matthias.harrer@oneclickmetal.com",
+    description="Module for reading file in the ocm file format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    python_requires='>=2.7',
+)
